fb6523bc0889910936c98620f67c4c02c80fcd78
Eric <ebagnaro@gmail.com>
2023-10-21 14:38:13 +0200

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Eric on 21/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
